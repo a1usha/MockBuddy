2c18cb8b568e9c38ca1e4d2093916ff697928342
Krichevskaya <v.krichevskaya@g.nsu.ru>
2021-03-21 13:23:26 +0700

    spy test

--- src/main/java/ru/nsu/sd/MockBuddy/examples/Main.java
+++ src/main/java/ru/nsu/sd/MockBuddy/examples/Main.java
@@ -27,32 +27,36 @@ public class Main {
 
         Test test = MockBuddy.mock(Test.class);
 
-        // (5 or 9) & (not (9 or 2) or 11) -> 5
-        MockBuddy.when(test.foo(and(or(equalsTo(5),equalsTo(9)), or(not(or(equalsTo(9),equalsTo(2))), equalsTo(11))))).thenReturn("Mocked");
-        System.out.println(test.foo(5));    // Mocked
-        System.out.println(test.foo(11));   // Null
+        SpyTest test2 = new SpyTest("kisa");
+        SpyTest spytest = MockBuddy.spy(test2);
 
-        MockBuddy.when(test.bar(anyInt(), or(equalsTo(123), equalsTo(333)))).thenReturn(11);
-        System.out.println(test.bar(333, 123)); // 11
-        System.out.println(test.bar(555, 333)); // 11
 
-        MockBuddy.when(test.boolTest(true)).invokeRealMethod();
-        System.out.println(test.boolTest(true));    // True
-        System.out.println(test.boolTest(false));   // Null
-
-        MockBuddy.when(test.tmp()).thenReturn("Mock_1");
-        MockBuddy.when(test.tmp()).thenReturn("Mock_2");
-        System.out.println(test.tmp()); // Mock_2
-
-        C c = new C();
-
-        c.setA(a);
-        c.setB(b);
-
-        MockBuddy.when(a.say()).thenReturn("Mock_A");
-        MockBuddy.when(b.say()).thenReturn("Mock_B");
-
-        System.out.println(c.sayAll()); // Mock_A Mock_B
+//        // (5 or 9) & (not (9 or 2) or 11) -> 5
+//        MockBuddy.when(test.foo(and(or(equalsTo(5),equalsTo(9)), or(not(or(equalsTo(9),equalsTo(2))), equalsTo(11))))).thenReturn("Mocked");
+//        System.out.println(test.foo(5));    // Mocked
+//        System.out.println(test.foo(11));   // Null
+//
+//        MockBuddy.when(test.bar(anyInt(), or(equalsTo(123), equalsTo(333)))).thenReturn(11);
+//        System.out.println(test.bar(333, 123)); // 11
+//        System.out.println(test.bar(555, 333)); // 11
+//
+//        MockBuddy.when(test.boolTest(true)).invokeRealMethod();
+//        System.out.println(test.boolTest(true));    // True
+//        System.out.println(test.boolTest(false));   // Null
+//
+//        MockBuddy.when(test.tmp()).thenReturn("Mock_1");
+//        MockBuddy.when(test.tmp()).thenReturn("Mock_2");
+//        System.out.println(test.tmp()); // Mock_2
+//
+//        C c = new C();
+//
+//        c.setA(a);
+//        c.setB(b);
+//
+//        MockBuddy.when(a.say()).thenReturn("Mock_A");
+//        MockBuddy.when(b.say()).thenReturn("Mock_B");
+//
+//        System.out.println(c.sayAll()); // Mock_A Mock_B
 
     }
 }
